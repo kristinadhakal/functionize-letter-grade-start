df33ed1a3cc8c0ced69856562d974e911f00e721
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-10-27 12:21:16 -0600

    10/27/22

--- main.js
+++ main.js
@@ -17,62 +17,24 @@ function btnClicked() {
 
   // Calculate & Output Letter Grades
   // English
-  let engLetter;
-  if (engPercent >= 80) {
-    engLetter = 'A';
-  } else if (engPercent >= 65) {
-    engLetter = 'B';
-  } else if (engPercent >= 55) {
-    engLetter = 'C';
-  } else if (engPercent >= 50) {
-    engLetter = 'D';
-  } else {
-    engLetter = 'F';
-  }
-  document.getElementById('eng-letter').innerHTML = engLetter;
-
-  // Social Studies
-  let socLetter;
-  if (socPercent >= 80) {
-    socLetter = 'A';
-  } else if (socPercent >= 65) {
-    socLetter = 'B';
-  } else if (socPercent >= 55) {
-    socLetter = 'C';
-  } else if (socPercent >= 50) {
-    socLetter = 'D';
-  } else {
-    socLetter = 'F';
-  }
-  document.getElementById('soc-letter').innerHTML = socLetter;
-
-  // Mathematics
-  let mathLetter;
-  if (mathPercent >= 80) {
-    mathLetter = 'A';
-  } else if (mathPercent >= 65) {
-    mathLetter = 'B';
-  } else if (mathPercent >= 55) {
-    mathLetter = 'C';
-  } else if (mathPercent >= 50) {
-    mathLetter = 'D';
-  } else {
-    mathLetter = 'F';
-  }
-  document.getElementById('math-letter').innerHTML = mathLetter;
-
-  // Science
-  let sciLetter;
-  if (sciPercent >= 80) {
-    sciLetter = 'A';
-  } else if (sciPercent >= 65) {
-    sciLetter = 'B';
-  } else if (sciPercent >= 55) {
-    sciLetter = 'C';
-  } else if (sciPercent >= 50) {
-    sciLetter = 'D';
-  } else {
-    sciLetter = 'F';
-  }
-  document.getElementById('sci-letter').innerHTML = sciLetter;
+  document.getElementById('eng-letter').innerHTML = calcMark(engPercent);
+  document.getElementById('soc-letter').innerHTML = calcMark(socPercent);
+  document.getElementById('math-letter').innerHTML = calcMark(mathPercent);
+  document.getElementById('sci-letter').innerHTML = calcMark(sciPercent);
 }
+
+function calcMark(percent){
+
+  if (percent >= 80) {
+    return 'A';
+  } else if (percent >= 65) {
+     return'B';
+  } else if (percent >= 55) {
+    return'C';
+  } else if (percent >= 50) {
+    return'D';
+  } else {
+    return'F';
+  }
+}
+
